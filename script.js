d1fc0ac1164f7b49eb27df112628035bad910417
Satapirom Khawatthu. <Satapirom.12@gmail.com>
2024-05-13 21:29:02 +0700

    control flow excercise 2, switch case

---
+++ script.js
@@ -0,0 +1,27 @@
+let dayNumber = 0;
+
+switch(true) {
+    case dayNumber === 0:
+        console.log("sunday");
+        break;
+    case dayNumber === 1:
+        console.log("monday");
+        break;
+    case dayNumber === 2:
+        console.log("tuesday");
+        break;
+    case dayNumber === 3:
+        console.log("wednesday");
+        break;
+    case dayNumber === 4:
+        console.log("thursday");
+        break;
+    case dayNumber === 5:
+        console.log("friday");
+        break;
+    case dayNumber === 6:
+        console.log("saturday");
+        break;
+    default:
+        console.log("Invalid day number")
+};
